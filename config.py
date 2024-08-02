9661cda56232ec18b799868c54ed105c4d0c8874
Shubham Panchal <Shubhp97@gmail.com>
2024-08-02 23:15:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "28238792"))
+API_HASH = getenv("API_HASH", "89b0d1f463a860525e1c117ccdef5aa3")
+BOT_TOKEN = getenv("BOT_TOKEN", "6360468548:AAG--HzheT_KQvGKYXM135XmGNumPIi0a2Y")
+OWNER_ID = int(getenv("OWNER_ID", "1236569704"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://shubhp97:RI9LlWYGuMIJFJ1w@mrpanchall.p0lnx7k.mongodb.net/?retryWrites=true&w=majority&appName=Mrpanchall")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
 FORCESUB = getenv("FORCESUB", "")
